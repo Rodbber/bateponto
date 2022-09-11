2ccd6b47539f0fa4ac29a5840d4539ebb697d4e4
Rodrigo Barbosa Bertini <rod.lemon@hotmail.com>
2022-09-11 14:06:51 -0300

    atualização com historico de pontos do cliente

--- app/Http/Controllers/WebControllers/Funcionario/FuncionarioPontoController.php
+++ app/Http/Controllers/WebControllers/Funcionario/FuncionarioPontoController.php
@@ -102,4 +102,85 @@ class FuncionarioPontoController extends Controller
             throw $th;
         }
     }
+
+    public function getHistorico(Request $request)
+    {
+        $user = $request->user();
+        $empresaFuncionario = EmpresaFuncionario::where('funcionario_user_id', $user->id)->first();
+
+        try {
+            $historico = FuncionarioPontoInicio::latest()->where('empresa_funcionario_id', $empresaFuncionario->id)->get();
+            $historico = $historico->map(function ($p) {
+                $p['tipo'] = 'ponto inicio';
+                $inicio = collect([$p]);
+                $intervalos = FuncIntervaloInicio::with('func_intervalo_fim')->where('funcionario_ponto_inicio_id', $p->id)->get();
+
+                $intervalos = $intervalos->map(function ($i) {
+                    $i['tipo'] = 'intervalo inicio';
+                    $intervaloInicio = collect([$i]);
+                    $intervaloFim = $i['func_intervalo_fim'];
+                    if ($intervaloFim) {
+                        $intervaloFim['tipo'] = 'intervalo fim';
+                        $intervaloInicio = $intervaloInicio->merge(collect([$intervaloFim]));
+                    }
+                    //$i->forget('func_intervalo_fim');
+                    return $intervaloInicio->all();
+                });
+
+                if ($intervalos) {
+                    //$inicio->push(['intervalos' => $intervalos]);
+                    $inicio = $inicio->merge(collect($intervalos));
+                }
+
+
+                $fim = FuncionarioPontoFim::where('funcionario_ponto_inicio_id', $p->id)->first();
+                if ($fim) {
+                    $fim['tipo'] = 'ponto fim';
+                    $inicio = $inicio->merge(collect([$fim]));
+                }
+
+
+
+
+                //$inicio = $inicio->merge($intervalos->all());
+
+                return $inicio;
+            });
+            $newArray = collect([]);
+            foreach ($historico as $key => $value) {
+                //$historico[$key]
+
+                foreach ($value as $key2 => $value2) {
+                    if (gettype($value2) == 'array') {
+                        foreach ($value2 as $key3 => $value3) {
+                            $newArray->push($value3);
+                        }
+                    } else {
+                        $newArray->push($value2);
+                    }
+                }
+            }
+
+            $sorted = $newArray->sortByDesc(function ($item, $key) {
+                return $item['created_at'];
+            })->values();
+
+            /* $intervalos26 = FuncIntervaloInicio::with('func_intervalo_fim')->where('funcionario_ponto_inicio_id', 26)->get();
+            $intervalosMap = $intervalos26->map(function ($i) {
+                $i['tipo'] = 'intervalo inicio';
+                $intervaloInicio = collect([$i]);
+                $intervaloFim = $i['func_intervalo_fim'];
+                if ($intervaloFim) {
+                    $intervaloFim['tipo'] = 'intervalo fim';
+                    $intervaloInicio = $intervaloInicio->merge(collect([$intervaloFim]));
+                }
+                //$i->forget('func_intervalo_fim');
+                return $intervaloInicio;
+            }); */
+
+            return response(['historico' => $sorted], 200);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
 }
--- routes/Funcionario/funcionarioPonto.php
+++ routes/Funcionario/funcionarioPonto.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/funcionario/verificarPonto', [FuncionarioPontoController::class, 'getStatusPonto']);
+    Route::get('/funcionario/ponto/historico', [FuncionarioPontoController::class, 'getHistorico']);
     Route::post('/funcionario/ponto/inicio', [FuncionarioPontoController::class, 'iniciar']);
     Route::post('/funcionario/ponto/intervalo/inicio', [FuncionarioPontoController::class, 'pausaInicio']);
     Route::post('/funcionario/ponto/intervalo/fim', [FuncionarioPontoController::class, 'pausaFim']);
