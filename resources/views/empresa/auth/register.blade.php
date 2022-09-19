97b48d53155c9b00c919630e6a40eb0474c72bf7
Murilo Bettin <murilobettin.cruz@outlook.com>
2022-09-19 17:46:34 -0300

    test

--- resources/views/empresa/auth/register.blade.php
+++ resources/views/empresa/auth/register.blade.php
@@ -30,7 +30,7 @@
             <div class="mt-4">
                 <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full"
+                <x-input id="password" class="block mt-1"
                                 type="password"
                                 name="password"
                                 required autocomplete="new-password" />
