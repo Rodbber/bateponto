3f347604083e44b746f21fc1e95db3f9252c4e59
Rodrigo Barbosa Bertini <rod.lemon@hotmail.com>
2022-09-20 21:51:34 -0300

    atualização para funcionar os loadings do app

--- app/Http/Controllers/WebControllers/Funcionario/FuncionarioPontoController.php
+++ app/Http/Controllers/WebControllers/Funcionario/FuncionarioPontoController.php
@@ -16,6 +16,7 @@ use App\Models\FuncionarioPontoFimPoligono;
 use App\Models\FuncionarioPontoInicio;
 use App\Models\FuncionarioPontoInicioPoligono;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class FuncionarioPontoController extends Controller
 {
@@ -29,8 +30,8 @@ class FuncionarioPontoController extends Controller
         try {
             $empresa_funcionario_id = $empresaFuncionario->id;
             $ponto = VerificarDentroPontosController::verificarSeEstaDentroDeAlgumPontoLocal($dados, $empresa_funcionario_id);
-            if(!$ponto){
-                return response(['message' => 'Nenhum ponto encontrado!', 'error' => 'Você não esta em nenhum ponto determinado em seu cadastro.'],400);
+            if (!$ponto) {
+                return response(['message' => 'Nenhum ponto encontrado!', 'error' => 'Você não esta em nenhum ponto determinado em seu cadastro.'], 400);
             }
             //$ponto_id = $ponto->id;
             $pontoInicio = FuncionarioPontoInicio::create(['empresa_funcionario_id' => $empresaFuncionario->id]);
@@ -59,7 +60,7 @@ class FuncionarioPontoController extends Controller
         try {
             $empresa_funcionario_id = $empresaFuncionario->id;
             $ponto = VerificarDentroPontosController::verificarSeEstaDentroDeAlgumPontoLocal($dados, $empresa_funcionario_id);
-            if(!$ponto){
+            if (!$ponto) {
                 return response(['message' => 'Nenhum ponto encontrado!', 'error' => 'Você não esta em nenhum ponto determinado em seu cadastro.'], 400);
             }
 
@@ -108,7 +109,7 @@ class FuncionarioPontoController extends Controller
         try {
             $empresa_funcionario_id = $empresaFuncionario->id;
             $ponto = VerificarDentroPontosController::verificarSeEstaDentroDeAlgumPontoLocal($dados, $empresa_funcionario_id);
-            if(!$ponto){
+            if (!$ponto) {
                 return response(['message' => 'Nenhum ponto encontrado!', 'error' => 'Você não esta em nenhum ponto determinado em seu cadastro.'], 400);
             }
             //$ponto_id = $ponto->id;
@@ -136,7 +137,7 @@ class FuncionarioPontoController extends Controller
         try {
             $empresa_funcionario_id = $empresaFuncionario->id;
             $ponto = VerificarDentroPontosController::verificarSeEstaDentroDeAlgumPontoLocal($dados, $empresa_funcionario_id);
-            if(!$ponto){
+            if (!$ponto) {
                 return response(['message' => 'Nenhum ponto encontrado!', 'error' => 'Você não esta em nenhum ponto determinado em seu cadastro.'], 400);
             }
             //$ponto_id = $ponto->id;
@@ -149,6 +150,77 @@ class FuncionarioPontoController extends Controller
                 'poligono_id' => $ponto->id,
             ]);
             return response(['message' => 'Intervalo finalizado.'], 200);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function getHistoricoComLimite(Request $request)
+    {
+        //return $request->all();
+        $dadosValidados = Validator::make($request->all(), [
+            'offset' => 'required|numeric',
+            'limit' => 'required|numeric',
+        ])->validated();
+        $user = $request->user();
+        $empresaFuncionario = EmpresaFuncionario::where('funcionario_user_id', $user->id)->first();
+
+
+
+        try {
+            $historico = FuncionarioPontoInicio::latest()->where('empresa_funcionario_id', $empresaFuncionario->id)->get();
+            $historico = $historico->map(function ($p) {
+                $p['tipo'] = 'ponto inicio';
+                $inicio = collect([$p]);
+                $intervalos = FuncIntervaloInicio::with('func_intervalo_fim')->where('funcionario_ponto_inicio_id', $p->id)->get();
+
+                $intervalos = $intervalos->map(function ($i) {
+                    $i['tipo'] = 'intervalo inicio';
+                    $intervaloInicio = collect([$i]);
+                    $intervaloFim = $i['func_intervalo_fim'];
+                    if ($intervaloFim) {
+                        $intervaloFim['tipo'] = 'intervalo fim';
+                        $intervaloInicio = $intervaloInicio->merge(collect([$intervaloFim]));
+                    }
+                    //$i->forget('func_intervalo_fim');
+                    return $intervaloInicio->all();
+                });
+
+                if ($intervalos) {
+                    //$inicio->push(['intervalos' => $intervalos]);
+                    $inicio = $inicio->merge(collect($intervalos));
+                }
+
+                $fim = FuncionarioPontoFim::where('funcionario_ponto_inicio_id', $p->id)->first();
+                if ($fim) {
+                    $fim['tipo'] = 'ponto fim';
+                    $inicio = $inicio->merge(collect([$fim]));
+                }
+
+                //$inicio = $inicio->merge($intervalos->all());
+
+                return $inicio;
+            });
+            $newArray = collect([]);
+            foreach ($historico as $key => $value) {
+                //$historico[$key]
+
+                foreach ($value as $key2 => $value2) {
+                    if (gettype($value2) == 'array') {
+                        foreach ($value2 as $key3 => $value3) {
+                            $newArray->push($value3);
+                        }
+                    } else {
+                        $newArray->push($value2);
+                    }
+                }
+            }
+
+            $sorted = $newArray->sortByDesc(function ($item, $key) {
+                return $item['created_at'];
+            })->values();
+            $dados = $sorted->skip($dadosValidados['offset'])->take($dadosValidados['limit'])->values();
+            return response(['offset' => $dadosValidados['offset'], 'limit' => $dadosValidados['limit'], 'length' => $dados->count(), 'historico' => $dados], 200);
         } catch (\Throwable $th) {
             throw $th;
         }
