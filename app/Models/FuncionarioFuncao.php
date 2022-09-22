e7f4da96343a20ce73bbdfb64803f9793af9391c
Rodrigo Barbosa Bertini <rod.lemon@hotmail.com>
2022-09-22 16:09:53 -0300

    atualização, erro na tolerancia na tabela funcao

--- app/Models/FuncionarioFuncao.php
+++ app/Models/FuncionarioFuncao.php
@@ -11,7 +11,7 @@ class FuncionarioFuncao extends Model
     use HasFactory;
     use SoftDeletes;
     protected $table = "funcionario_funcoes";
-    protected $fillable = ['empresa_funcionario_id', 'funcao', 'inicio', 'fim'];
+    protected $fillable = ['empresa_funcionario_id', 'funcao', 'inicio', 'tolerancia', 'fim'];
 
     public function empresa_funcionario(){
         return $this->belongsTo(EmpresaFuncionario::class, 'empresa_funcionario_id');
