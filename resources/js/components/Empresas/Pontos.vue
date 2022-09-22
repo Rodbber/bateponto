115087b8ec275ae20ea3ee08ecb97fbf41f972ab
Murilo Bettin <murilobettin.cruz@outlook.com>
2022-09-22 16:32:50 -0300

    test3

--- resources/js/components/Empresas/Pontos.vue
+++ resources/js/components/Empresas/Pontos.vue
@@ -313,9 +313,9 @@ import "leaflet/dist/leaflet.css";
 import * as L from "leaflet";
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: "leaflet/dist/images/marker-icon-2x.png",
-  iconUrl: "leaflet/dist/images/marker-icon.png",
-  shadowUrl: "leaflet/dist/images/marker-shadow.png"
+  iconRetinaUrl: "node_modules/leaflet/dist/images/marker-icon-2x.png",
+  iconUrl: "node_modules/leaflet/dist/images/marker-icon.png",
+  shadowUrl: "node_modules/leaflet/dist/images/marker-shadow.png"
 });
 import "leaflet-geosearch/dist/geosearch.css";
 import "leaflet-path-transform";
